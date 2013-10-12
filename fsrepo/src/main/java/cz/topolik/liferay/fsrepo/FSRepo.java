5f079eb5805d79a6a227a0551c4dc2ec75b1fd1e
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2013-10-12 21:43:58 +0200

    2.0 New FSRepo class

---
+++ fsrepo/src/main/java/cz/topolik/liferay/fsrepo/FSRepo.java
@@ -0,0 +1,12 @@
+package cz.topolik.liferay.fsrepo;
+
+import com.liferay.portal.kernel.repository.BaseRepositoryImpl;
+
+/**
+ * @author Tomas Polesovsky
+ */
+public class FSRepo extends BaseRepositoryImpl {
+
+    // TBD
+
+}
